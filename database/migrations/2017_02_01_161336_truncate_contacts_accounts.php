6fe2ad425bdd7d742d685ca888677dfa7572a7e8
Dustin Weaver <dustin.weaver@payscape.com>
2017-02-01 10:26:25 -0600

    fix migration class name

--- database/migrations/2017_02_01_161336_truncate_contacts_accounts.php
+++ database/migrations/2017_02_01_161336_truncate_contacts_accounts.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class TruncateContactsAndAccounts extends Migration
+class TruncateContactsAccounts extends Migration
 {
     /**
      * Run the migrations.
